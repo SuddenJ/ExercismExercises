45ea559aa276b8d5f8f88e405f985abcf6df9135
Josh <Josh@DESKTOP-SE7CCLL>
2022-07-26 10:37:45 +0100

    Add project files.

---
+++ HelloWorldGitTest/Darts.cs
@@ -0,0 +1,31 @@
+﻿namespace ExercismExercises
+{
+    using System;
+
+    public static class Darts
+    {
+        public static int Score(double x, double y)
+        {
+            double absX = Math.Abs(x);
+            double absY = Math.Abs(y);
+            double XYdart = Math.Sqrt(Math.Pow(absY, 2) + Math.Pow(absX, 2));
+
+            if (XYdart > 10)
+            {
+                return 0;
+            }
+            else if (XYdart > 5 && XYdart <= 10)
+            {
+                return 1;
+            }
+            else if (XYdart > 1 && XYdart <= 5)
+            {
+                return 5;
+            }
+            else
+            {
+                return 10;
+            }
+        }
+    }
+}
